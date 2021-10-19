74964abfa8ef04971d1a389a942295e229247ae3
andres <worldmet.info@gmail.com>
2021-10-19 14:16:08 +0300

    loendite kasutamine

--- ee/spiridon/matem_avaldused.py
+++ ee/spiridon/matem_avaldused.py
@@ -5,6 +5,16 @@ Created on 18. okt 2021
 '''
 1 + 2
 print(1 + 2)
-one = 4
-two = 26
+one = '4'
+two = '26'
 print(one + two)
+penta = [1, 2, 3, 4, 5]
+print(penta)
+heksa = [1, 2, 3, 4, 5, 6]
+print(heksa + penta)
+penta1 = ('a', 'b', 'c', 'd', 'e')
+heksa1 = ['f', 'g', 'h', 'i', 'j', 'k']
+print(heksa1[3])
+# heksa1 toon valja 4. elemendi , kasutan [ ] sulge , lugemine algab alati 0-st.
+print(penta1[1])
+# penta1,toon valja 1.elemendi,kasutan [ ]sulge .
